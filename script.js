c6af22c704fcbb075359219e9549ba08655a505e
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-30 00:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	let s="";
+	for(let i=str.length;i>=0;i--){
+		s=s+str.charAt(i);
+	}
+	if(s===str){
+	return true;
+	}
+	return false;
 
 }
 module.exports = palindrome
